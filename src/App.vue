7cd86213576712a09c0aa0ba609de43d6acda5eb
MathPasha <nathan.simonnet@gmail.com>
2025-02-06 18:32:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <main>
+        <div class="task-iput-container">
+            <label for="inputText">Enter a new task</label>
+            <input type="text" id="inputText" v-model="newTask" @keyup.enter="addNewTask">
+        </div>
+        <div class="task-list-container">
+            <ul>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+            </ul>
+        </div>
+
+    </main>
+</template>
+<!-- ---------------------------- -->
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+const newTask = ref("");
+
+const addNewTask = () => {
+    console.log(newTask.value);
+
+    newTask.value = ""
+}
+
+</script>
+<!-- ---------------------------- -->
+<style></style>
